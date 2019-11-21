a366abdc4fc2f9dea628ca0eca613ad6a4d5a369
Clara238 <57843089+Clara238@users.noreply.github.com>
2019-11-21 09:00:06 +0100

    Add files via upload

---
+++ ball.js
@@ -0,0 +1,11 @@
+const hexNum = [0,1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F'];
+let colorChange = document.querySelector('$colChange');
+let yes = true;
+
+if (yes=true) {
+    function getit() {
+    
+    let random = Math.floor(Math.random()*hexNum.length);
+    colorChange += hexNum [random];
+    }
+}
